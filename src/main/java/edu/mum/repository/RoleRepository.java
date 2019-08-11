0aca07c925e22ccec5b69365be57637571df0924
jorge <jorge_930614@hotmail.com>
2019-08-10 23:24:15 -0500

    Services Impl added

--- src/main/java/edu/mum/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package edu.mum.repository;
-
-import edu.mum.domain.Role;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RoleRepository extends CrudRepository<Role, Long> {
-}
